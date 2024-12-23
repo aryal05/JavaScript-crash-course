8d7556993c8d47c7d8de0df50a6615e109e85762
aryal05 <aryal.rajat05@gmail.com>
2024-12-23 20:38:47 +0545

    prototype inheritance

--- Advancejs/advance.js
+++ Advancejs/advance.js
@@ -15,3 +15,24 @@ var ans = (function(){
 //ans.setter to access the val
 
 //Prototype
+obj ={
+    name: 'Darling'
+}
+// WhenEver an object is called it contains an extra property called prototype.
+
+
+//Prototype Inheritance
+var human = {
+    canTalk: true,
+    canFly:false,
+    haveEmotions: true,
+}
+
+var me = {
+    canMakeAwesomeWebiste: true,
+    canWriteGoodCode: true,
+    great:true
+}
+me.__proto__ = human;
+
+
