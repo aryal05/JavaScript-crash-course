4fec39e9ccae075a8aab299aa0a5389581188803
aryal05 <aryal.rajat05@gmail.com>
2024-12-23 21:12:57 +0545

    this , call, bind in javaScript

--- Advancejs/advance.js
+++ Advancejs/advance.js
@@ -36,3 +36,48 @@ var me = {
 me.__proto__ = human;
 
 
+// this 
+// It is a Special KeyWord
+// In global scope this refers to the global object and gives the value window (console.log(this))
+
+// console.log(this)
+
+// In function Scoped also it gives window
+function test(){
+    console.loog(this)
+}
+
+// this in methods  give the value of parent object
+var objs = {
+    age:12,
+    name: function(){
+        console.log(this)
+    }
+
+}
+objs.name()
+
+// In event listener fo the below case it refers to the button
+var button = document.querySelector("button");
+button.addEventListener('click', function(){
+    this.style.color ="red"
+    // console.log(this)
+})
+
+// call apply bind
+// call apply bind are methods of function object
+// they are used to change the context of a function
+
+function darling (val1,val2){
+    console.log(this,val1,val2)
+}
+ var babe = {
+    name: "babe"
+ }
+ darling.call(babe,1,2) // call method is used to change the context of a function
+
+// for apply
+darling.apply(babe,[1,2]) // apply method is used to change the context of a
+
+var bindFunction = darling.bind(babe)
+bindFunction(1,2) // bind method is used to change the context of a function
